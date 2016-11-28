5413264298bf8a52478a22a1bf35bd49fa10e654
Simon Vergauwen <simon.vergauwen@appfoundry.be>
2016-11-28 14:14:29 +0100

    2 spaces

--- adaptercommands/src/main/java/com/hannesdorfmann/adaptercommands/command/ThreadSafeDiffCommandsCalculator.java
+++ adaptercommands/src/main/java/com/hannesdorfmann/adaptercommands/command/ThreadSafeDiffCommandsCalculator.java
@@ -18,9 +18,7 @@ package com.hannesdorfmann.adaptercommands.command;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-
 import com.hannesdorfmann.adaptercommands.ItemChangedDetector;
-
 import java.util.List;
 
 /**
@@ -32,37 +30,42 @@ import java.util.List;
  */
 public class ThreadSafeDiffCommandsCalculator<T> extends DiffCommandsCalculator<T> {
 
-    public ThreadSafeDiffCommandsCalculator() {
-        super();
-    }
+  public ThreadSafeDiffCommandsCalculator() {
+    super();
+  }
 
-    public ThreadSafeDiffCommandsCalculator(boolean itemRangeInsertedOnFirstDiff) {
-        super(itemRangeInsertedOnFirstDiff);
-    }
+  public ThreadSafeDiffCommandsCalculator(boolean itemRangeInsertedOnFirstDiff) {
+    super(itemRangeInsertedOnFirstDiff);
+  }
 
-    public ThreadSafeDiffCommandsCalculator(ItemChangedDetector<T> detector) {
-        super(detector);
-    }
+  public ThreadSafeDiffCommandsCalculator(ItemChangedDetector<T> detector) {
+    super(detector);
+  }
 
-    public ThreadSafeDiffCommandsCalculator(boolean itemRangeInsertedOnFirstDiff,
-                                            @Nullable ItemChangedDetector<T> detector) {
-        super(itemRangeInsertedOnFirstDiff, detector);
-    }
+  public ThreadSafeDiffCommandsCalculator(boolean itemRangeInsertedOnFirstDiff,
+      @Nullable ItemChangedDetector<T> detector) {
+    super(itemRangeInsertedOnFirstDiff, detector);
+  }
 
-    @Override
-    public synchronized List<AdapterCommand> diff(@NonNull List<T> oldList, @NonNull List<T> newList) {
-        return super.diff(oldList, newList);
-    }
+  /**
+   * This method calculates the difference between two lists.
+   * This call is thread safe
+   *
+   * @param newList The new items that we use to calculate the difference
+   * @return List of commands
+   */
+  @Override public synchronized List<AdapterCommand> diff(@NonNull List<T> oldList, @NonNull List<T> newList) {
+    return super.diff(oldList, newList);
+  }
 
-    /**
-     * This method calculates the difference of previous list of items and the new list.
-     * This call is thread safe
-     *
-     * @param newList The new items that we use to calculate the difference
-     * @return List of commands
-     */
-    @Override
-    public synchronized List<AdapterCommand> diff(@NonNull List<T> newList) {
-        return super.diff(newList);
-    }
+  /**
+   * This method calculates the difference of previous list of items and the new list.
+   * This call is thread safe
+   *
+   * @param newList The new items that we use to calculate the difference
+   * @return List of commands
+   */
+  @Override public synchronized List<AdapterCommand> diff(@NonNull List<T> newList) {
+    return super.diff(newList);
+  }
 }
